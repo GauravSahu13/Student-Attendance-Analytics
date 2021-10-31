4d676f43c3281842c06a1efa3b842398c5ae3b2b
Gaurav <gauravsahu7045@gmail.com>
2021-11-01 00:33:54 +0530

    Indentation fixing Comments

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 from flask import *
-from flask_sqlalchemy import SQLAlchemy, request
+from flask import request
 import csv
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
+    # Home Page consists of Student Management System
     return render_template("home.html")
 
-# branch_name = str(input("Which branch student you want? "))
+
 @app.route("/view_student")
 def view_student():
+    # View Student Details in Each Branch after analysis
     return render_template("View_Student.html")
+
 
 @app.route("/extc")
 def extc():
     return render_template("extc.html")
-
 @app.route("/it")
 def IT():
     return render_template("IT.html")
-
 @app.route("/mech")
 def mech():
     return render_template("mech.html")
-
 @app.route("/comps")
 def comps():
     return render_template("comps.html")
 
-@app.route("/add_student", methods = ["GET","POST"])
+
+@app.route("/add_student", methods=["GET", "POST"])
 def add_student():
+    # Register page
     if request.method == "POST":
         Name = request.form.get("Name")
         Gender = request.form.get("Gender")
@@ -38,6 +41,7 @@ def add_student():
         Branch = request.form.get("Branch")
         Gmail = request.form.get("Gmail")
 
+        # saving data into csv file
         with open('ATT.csv', 'a') as readFile:
             fieldnames = ['Name','Gender', 'Eng_Year','Branch', 'Gmail']
             writer = csv.DictWriter(readFile, fieldnames=fieldnames)
@@ -48,8 +52,10 @@ def add_student():
 
     return render_template("add_student_info.html")
 
+
 @app.route("/mark_attendance", methods = ["GET","POST"])
 def mark_student():
+    # Attendance Portal Page
     if request.method == "POST":
         Date = request.form.get("Date")
         Branch = request.form.get("Branch")
@@ -58,6 +64,7 @@ def mark_student():
         Subjects = request.form.get("Subjects")
         Attendance = request.form.get("Attendance")
 
+        # saving attendance into csv file
         with open('mark.csv', 'a') as readFile:
             fieldnames = ['Date', 'Branch', 'Eng_Year', 'Roll_No', 'Subjects', 'Attendance']
             writer = csv.DictWriter(readFile, fieldnames=fieldnames)
@@ -67,22 +74,21 @@ def mark_student():
             writer.writerow({'Date': Date, 'Branch': Branch, 'Eng_Year': Eng_Year, 'Roll_No': Roll_No, 'Subjects': Subjects, 'Attendance': Attendance})
     return render_template("mark_attendance.html")
 
+
 @app.route("/analysis")
 def Analysis():
+    # Graphical Representation of Edu_yearly,monthly,weekly,subjectwise
     return render_template("analysis.html")
 
 @app.route("/yearwise")
 def yearAnalysis():
     return redirect(url_for('static', filename='FY.png'))
-
 @app.route("/monthwise")
 def monthAnalysis():
     return redirect(url_for('static', filename='TMSY.png'))
-
 @app.route("/weekwise")
 def weekAnalysis():
     return redirect(url_for('static', filename='W1FoY.png'))
-
 @app.route("/subjectwise")
 def subAnalysis():
     return redirect(url_for('static', filename='SOM1.png'))
